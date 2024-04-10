47a0e86269fbf127f23ce43fbea13e5643903cbc
augentism <thecreamcheesemonkey@gmail.com>
2024-04-10 15:10:58 -0400

    Fixed Localization Issues and added auto export

--- zzexport.py
+++ zzexport.py
@@ -8,12 +8,17 @@ zip_name = "NewWorldCrops"
 directory_name = "assets"
 fullpath = os.path.join(cwd,directory_name)
 #print(fullpath)
-if os.path.isfile("./" + zip_name):
+if os.path.isfile("./" + zip_name +".zip"):
     print("found zip, deleting...")
-    os.remove("./NewWorldCrops.zip")
+    os.remove("./" + zip_name +".zip")
 
 
 shutil.make_archive(zip_name, 'zip', fullpath)
 
 with zipfile.ZipFile("NewWorldCrops.zip", mode='a') as zf:
     zf.write("./modinfo.json")
+
+if os.path.isfile("C:\\Users\\Calvin\\AppData\\Roaming\\Vintagestory\\Mods\\" + zip_name + ".zip"):
+    os.remove("C:\\Users\\Calvin\\AppData\\Roaming\\Vintagestory\\Mods\\" + zip_name + ".zip")
+
+shutil.copy("./" + zip_name +".zip", "C:\\Users\\Calvin\\AppData\\Roaming\\Vintagestory\\Mods\\")
